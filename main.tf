95784fdb513dc0ee20586209b024da8fa763afd9
Sai Krishna Thotakuri <thotakuri01@gmail.com>
2025-10-17 12:03:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "one" {
-  ami           = "ami-0360c520857e3138f"
+  ami           = "ami-0360c520857e3138f "
   instance_type = "t3.micro"
 
   tags = {
