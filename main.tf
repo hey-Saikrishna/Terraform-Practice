17a552061d417b57361c0aa2b06ec1eb35f41d0b
Sai Krishna Thotakuri <thotakuri01@gmail.com>
2025-10-24 15:18:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,1 @@
-provider "aws" {
-  region = "us-east-1"
-}
 
-resource "aws_instance" "one" {
-  ami           = "ami-0360c520857e3138f"
-  instance_type = "t3.micro"
-
-  tags = {
-    Name = "sai_server"
-  }
-}
